2b9589e145755df06ab6b52af6291efd60bb1306
Alice <paulorobertobernal68@gmail.com>
2025-09-08 15:16:59 -0300

    :3

---
+++ pasta/pre-form/app/etapa4.tsx
@@ -0,0 +1,127 @@
+import { useState } from "react";
+import CheckBox from "expo-checkbox";
+import { Link, useRouter } from "expo-router";
+import { TouchableOpacity } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
+
+export default function Etapa1() {
+  /*const [isChecked1, setChecked1] = useState(false)
+  const [isChecked2, setChecked2] = useState(false)
+  const router = useRouter();
+
+  function prosseguir(){
+      if(isChecked1 == true && isChecked2 == true){
+        router.replace('/etapa2')
+      }else if(isChecked1 == true || isChecked2 == true){
+        alert("É NECESSÁRIO ACEITAR TODOS ACIMA!")
+      }else{
+        alert("PARA CONTINUARES, É NECESSÁRIO ACEITAR TODOS ACIMA!")
+      }
+  }*/
+
+  return (
+    <View style={estilo.container}>
+      <View style={estilo.imageContainer}>
+        <Image
+          source={require(`./img/proxy-image.jpg`)}
+          style={estilo.img}
+        />
+      </View>
+      
+      <View>
+         
+      </View>
+
+      
+      <View style={estilo.btnContainer}>
+        <TouchableOpacity 
+        style={estilo.button}
+        onPressIn={() => {
+          prosseguir()
+        }}>
+          <Text style={estilo.buttonText}>Prosseguir</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const estilo = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    borderWidth: 2,
+    borderRadius: 20,
+    marginVertical: 32,
+    alignItems: 'center',
+    marginHorizontal: 16,
+    backgroundColor: '#E0F7E9',
+  },
+
+  imageContainer: {
+    marginBottom: 20,
+  },
+  
+  btnContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+
+  img: {
+    width: 200,
+    height: 200,
+    backgroundColor: "null"
+  },
+  
+  textContainer: {
+    marginBottom: 30,
+    alignItems: 'center',
+  },
+  
+  title: {
+    fontSize: 50,
+    color: 'green',
+    textAlign: 'center',
+  },
+
+  subtitle: {
+    fontSize: 15,
+    textAlign: 'center',
+  },
+
+  checkboxContainer: {
+    width: '100%',
+    marginBottom: 30,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+  },
+
+  checkbox: {
+    marginTop: 3,
+    marginRight: 10,
+    borderRadius: 30,
+  },
+
+  checkboxText: {
+    flex: 1,
+  },
+
+  button: {
+    width: '100%',
+    borderRadius: 10,
+    paddingVertical: 12,
+    alignItems: 'center',
+    paddingHorizontal: 30,
+    backgroundColor: 'green',
+  },
+  
+  buttonText: {
+    fontSize: 16,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+
+  link: {
+    color: 'green',
+  }
+});
